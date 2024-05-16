dd3ad915665abce3dcc4ae93a5aa65c11a7c0e75
Dania Garcia <daniagm75@gmail.com>
2024-05-16 12:49:43 -0600

    Ruta con nombre

--- my_store/urls.py
+++ my_store/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
+
+def hello(request):
+    return HttpResponse('<H1>HOLA 💗</H1>')
 
 urlpatterns = [
     path('', include('products.urls')),
     path('admin/', admin.site.urls),
+    path('hello/', hello, name='hello'),
 ]
