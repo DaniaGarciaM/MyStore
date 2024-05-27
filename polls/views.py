1ae51c73c4bdda2572312c68aa79436566b96baf
Dania Garcia <daniagm75@gmail.com>
2024-05-27 13:51:51 -0600

    Implementación de Votos

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Question, Choice
 # Create your views here.
 def home(request):
@@ -14,11 +14,23 @@ def get_polls(request):
     )
 
 def vote(request, q_id):
-    q = Question.objects.get(pk=q_id)
+    q = get_object_or_404(Question, pk=q_id)
+    if request.method == "POST":
+        choice_id = request.POST.get('choice')
+        choice = q.choice_set.get(pk=choice_id)
+        choice.votes += 1
+        choice.save()
+        return redirect('poll:result', q_id)
     return render(request, 'poll/vote.html', {
         "question": q
     })
     
 
 def result(request, q_id):
-    pass
+    try:
+        q = Question.objects.get(pk=q_id)
+    except Question.DoesNotExist:
+        return redirect('poll:get_polls')
+    return render(request, 'poll/result.html', {
+        "question":q
+    })
